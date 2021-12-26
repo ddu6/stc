be432718600e79208cf8fabc292782d4dcfa393b
DDU <ddu6@protonmail.com>
2021-12-26 17:13:42 +0800

    update

--- test/main.js
+++ test/main.js
@@ -5,9 +5,7 @@ const example=`{id flt, style margin:1em 0, theorem [
 ]}
 ['The first successful proof of '{href #flt, a[theorem FLT]}' was released in 1994 by Andrew Wiles.']
 {style height:100vh}`
-;(async()=>{
-    const result=await compile(example)
-    if(result!==undefined){
-        document.body.append(result.documentFragment)
-    }
-})()
+const result=await compile(example)
+if(result!==undefined){
+    document.body.append(result.documentFragment)
+}
