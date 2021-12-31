1753070328154f8b397921f515f2912575250a4c
DDU <ddu6@protonmail.com>
2022-01-01 00:30:31 +0800

    update

--- test/main.js
+++ test/main.js
@@ -5,7 +5,7 @@ const example=`{id flt, style margin:1em 0, theorem [
 ]}
 ['The first successful proof of '{href #flt, a[theorem FLT]}' was released in 1994 by Andrew Wiles.']
 {style height:100vh}`
-const result=await compile(example)
+const result=await compile(example,location.href)
 if(result!==undefined){
     document.body.append(result.documentFragment)
 }
