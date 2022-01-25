c20ac37520b2a715710025b04862753ef92b5052
DDU <ddu6@protonmail.com>
2022-01-25 22:47:34 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,3 +1,3 @@
 import {compileURLs} from '../mod.js'
-const result = window.result = await compileURLs([new URL('./main.stdn', import.meta.url).href])
+const result = window.result = await compileURLs([new URL('./main.urls', import.meta.url).href])
 document.body.append(result.documentFragment)
