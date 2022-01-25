1fdf155100b88d4fff699315a53463c670a33e50
DDU <ddu6@protonmail.com>
2022-01-25 20:06:17 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,10 +1,10 @@
 import {compile} from '../mod.js'
-const example = `{id flt, style margin:1em 0, theorem [
+const example = `{id flt, theorem [
     'Let n be an integer greater than 2, then there are no positive integers a,b,c satisfying'
-    {style text-align:center, 'a^n+b^n=c^n.'}
+    {style text-align: center, 'a^n+b^n=c^n.'}
 ]}
-['The first successful proof of '{href #flt, a[theorem FLT]}' was released in 1994 by Andrew Wiles.']
-{style height:100vh}`
+['The first successful proof of '{href #flt, a [theorem FLT]}' was released in 1994 by Andrew Wiles.']
+{style height: 100vh}`
 const result = window.result = await compile([{
     value: example,
     url: location.href
